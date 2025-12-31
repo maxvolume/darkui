d59af08696ab24354fa057f4a52d92ebcf656289
maxvolume321 <ben@schonbeck.io>
2025-12-31 16:31:37 -0500

    Basic styling with Tailwind

---
+++ public/layouts/index.tsx
@@ -0,0 +1,21 @@
+import type { PropsWithChildren } from "react";
+import clsx from "clsx";
+
+import "../styles/global.css";
+
+interface LayoutProps extends PropsWithChildren {
+  className?: string;
+}
+
+export default function Layout({ children, className }: LayoutProps) {
+  return (
+    <div
+      className={clsx(
+        "flex flex-col justify-center items-center w-full min-h-screen",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
