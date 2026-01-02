133f9a81450aa01b42d94b7a1277c46b7d334cb9
maxvolume321 <ben@schonbeck.io>
2026-01-02 17:31:41 -0500

    Start adding tanstack router

---
+++ public/routes/router.tsx
@@ -0,0 +1,8 @@
+import {
+  Outlet,
+  Link,
+  createRouter,
+  createRoute,
+  createRootRoute,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
